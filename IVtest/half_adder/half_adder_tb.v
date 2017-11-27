84f56e7db78a71085d39c4f3614c3e3f93bfc733
firen777 <hivemindac1995@gmail.com>
2017-11-26 21:58:32 -0800

    half_adder working. Add Icarus. ModelSim Cleaning in Progress.

---
+++ IVtest/half_adder/half_adder_tb.v
@@ -0,0 +1,19 @@
+`include "prj_definition.v"
+module HALF_ADDER_TB;
+
+  reg A,B;
+  wire Y,C;
+
+  HALF_ADDER HALF_ADDER_INST(Y,C,A,B);
+
+  initial begin
+    $dumpfile("half_adder.vcd");
+    $dumpvars(0, HALF_ADDER_TB);
+    A=0;B=0;
+    #5 A=1; B=0;
+    #5 A=0; B=1;
+    #5 A=1; B=1;
+    $finish;
+  end
+
+endmodule
