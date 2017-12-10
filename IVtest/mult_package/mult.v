ef7413b3977b803f2f749413c5919fa372a51012
firen777 <hivemindac1995@gmail.com>
2017-12-09 18:40:11 -0800

    multiplier init

---
+++ IVtest/mult_package/mult.v
@@ -0,0 +1,63 @@
+`include "rc_add_sub_32.v"
+// Name: mult.v
+// Module: MULT32 , MULT32_U
+//
+// Output: HI: 32 higher bits
+//         LO: 32 lower bits
+//
+//
+// Input: A : 32-bit input
+//        B : 32-bit input
+//
+// Notes: 32-bit multiplication
+//
+//
+// Revision History:
+//
+// Version	Date		Who		email			note
+//------------------------------------------------------------------------------------------
+//  1.0     Sep 10, 2014	Kaushik Patra	kpatra@sjsu.edu		Initial creation
+//------------------------------------------------------------------------------------------
+`include "prj_definition.v"
+
+module MULT32(HI, LO, A, B);
+// output list
+output [31:0] HI;
+output [31:0] LO;
+// input list
+input [31:0] A;
+input [31:0] B;
+
+// TBD
+
+endmodule
+
+module MULT32_U(HI, LO, A, B);
+// output list
+output [31:0] HI;
+output [31:0] LO;
+// input list
+input [31:0] A;
+input [31:0] B;
+
+// TBD
+
+endmodule
+
+//32 bit parallel AND gates.
+module AND32_ARRAY (Y, A, B);
+  // output list
+  output [31:0] Y;
+
+  // input list
+  input [31:0] A;
+  input [31:0] B;
+
+  genvar i;
+  generate
+    for (i=0;i<32;i=i+1)
+    begin: and32_array_gen
+      and and_inst(Y[i],A[i],B[i]);
+    end
+  endgenerate
+endmodule
---
+++ IVtest/mult_package/rc_add_sub_32.v
@@ -0,0 +1,73 @@
+`include "full_adder.v"
+// Name: rc_add_sub_32.v
+// Module: RC_ADD_SUB_32
+//
+// Output: Y : Output 32-bit
+//         CO : Carry Out
+//
+//
+// Input: A : 32-bit input
+//        B : 32-bit input
+//        SnA : if SnA=0 it is add, subtraction otherwise
+//
+// Notes: 32-bit adder / subtractor implementaiton.
+//
+//
+// Revision History:
+//
+// Version	Date		Who		email			note
+//------------------------------------------------------------------------------------------
+//  1.0     Sep 10, 2014	Kaushik Patra	kpatra@sjsu.edu		Initial creation
+//------------------------------------------------------------------------------------------
+`include "prj_definition.v"
+
+module RC_ADD_SUB_64(Y, CO, A, B, SnA);
+// output list
+output [63:0] Y;
+output CO;
+// input list
+input [63:0] A;
+input [63:0] B;
+input SnA;
+
+// TBD
+
+endmodule
+
+module RC_ADD_SUB_32(Y, CO, A, B, SnA);
+  // output list
+  output [`DATA_INDEX_LIMIT:0] Y;
+  output CO;
+  // input list
+  input [`DATA_INDEX_LIMIT:0] A;
+  input [`DATA_INDEX_LIMIT:0] B;
+  input SnA;
+
+  wire [`DATA_INDEX_LIMIT:0] carryWire; //ripple carry wire
+  wire [`DATA_INDEX_LIMIT:0] subWire; //B xor SnA, for subtraction
+
+
+  //XOR array for subtraction:
+  genvar i;
+  generate
+    for (i=0; i<32; i=i+1)
+    begin: add_sub_32_xor_gen
+      xor xor_inst(subWire[i],B[i],SnA);
+    end
+  endgenerate
+
+  //module FULL_ADDER(S,CO,A,B, CI);
+  //first full_adder
+  FULL_ADDER F1(Y[0],carryWire[0],A[0],subWire[0],SnA);
+
+  //full_adder array [1..30]
+  generate
+    for (i=1; i<32; i=i+1)
+    begin: add_sub_32_adder_gen
+      FULL_ADDER FULL_ADDER_INST(Y[i],carryWire[i],A[i],subWire[i],carryWire[i-1]);
+    end
+  endgenerate
+
+  assign CO = carryWire[31];
+
+endmodule
