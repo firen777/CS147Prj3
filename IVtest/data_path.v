2f672bff1e15ef6c1b41bd848318074922fc4920
firen777 <hivemindac1995@gmail.com>
2017-12-11 13:54:39 -0800

    data path WIP3

--- IVtest/data_path.v
+++ IVtest/data_path.v
@@ -27,16 +27,16 @@
   module DATA_PATH(DATA_OUT, ADDR, ZERO, INSTRUCTION, DATA_IN, CTRL, CLK, RST);
 
   // output list
-  output [`ADDRESS_INDEX_LIMIT:0] ADDR;
+  output [`ADDRESS_INDEX_LIMIT:0] ADDR; //[25:0]
   output ZERO;
-  output [`DATA_INDEX_LIMIT:0] DATA_OUT, INSTRUCTION;
+  output [`DATA_INDEX_LIMIT:0] DATA_OUT, INSTRUCTION; //[31:0]
 
   // input list
-  input [`CTRL_WIDTH_INDEX_LIMIT:0]  CTRL;
+  input [`CTRL_WIDTH_INDEX_LIMIT:0]  CTRL; //[31:0]
   input CLK, RST;
-  input [`DATA_INDEX_LIMIT:0] DATA_IN;
+  input [`DATA_INDEX_LIMIT:0] DATA_IN; //[31:0]
 
-  //pc_load
+  //[0]pc_load
   wire [`DATA_INDEX_LIMIT:0] pc_Q;
     wire [`DATA_INDEX_LIMIT:0] pc_D;
     wire pc_load;
@@ -44,25 +44,25 @@
     wire [`DATA_INDEX_LIMIT:0] pc_plus_one;
     RC_ADD_SUB_32 pc_increment_inst(.Y(pc_plus_one), .CO(), .A(pc_Q), .B(32'h00000001), .SnA(1'b0));
 
-  //pc_sel_1
+  //[1]pc_sel_1
   wire [`DATA_INDEX_LIMIT:0] pc_sel_1_out;
     wire pc_sel_1;
     wire [`DATA_INDEX_LIMIT:0] pc_sel_1_i0;
     MUX32_2x1 pc_sel_1_mux_inst(.Y(pc_sel_1_out), .I0(pc_sel_1_i0), .I1(pc_plus_one), .S(pc_sel_1));
 
-  //pc_sel_2
+  //[2]pc_sel_2
   wire [`DATA_INDEX_LIMIT:0] pc_sel_2_out;
     wire pc_sel_2;
     wire [`DATA_INDEX_LIMIT:0] pc_sel_2_i1;
     MUX32_2x1 pc_sel_2_mux_inst(.Y(pc_sel_2_out), .I0(pc_sel_1_out), .I1(pc_sel_2_i1), .S(pc_sel_2));
 
-  //pc_sel_3
+  //[3]pc_sel_3
   wire [`DATA_INDEX_LIMIT:0] pc_sel_3_out;
     wire pc_sel_3;
     wire [`DATA_INDEX_LIMIT:0] pc_sel_3_i0;
     MUX32_2x1 pc_sel_3_mux_inst(.Y(pc_sel_3_out), .I0(pc_sel_3_i0), .I1(pc_sel_2_out), .S(pc_sel_3));
 
-  //ir_load
+  //[4]ir_load
   wire ir_load;
     wire [`DATA_INDEX_LIMIT:0] ir_Q;
     wire [`DATA_INDEX_LIMIT:0] ir_D;
@@ -83,12 +83,35 @@
     assign addr = ir_Q[25:0];
 
 
-  //mem_r
+  //[5]mem_r
 
-  //mem_w
+  //[6]mem_w
 
-  //r1_sel_1
+  //[7]r1_sel_1
   wire r1_sel_1;
+    wire [31:0] r1_sel_1_out;
+    MUX32_2x1 r1_sel_1_mux_inst(.Y(r1_sel_1_out), .I0({27'b0,rs}), .I1(32'b0), .S(r1_sel_1));
+
+  //[8]reg_r
+  //[9]reg_w
+  //[10]wa_sel_1
+  //[11]wa_sel_2
+  //[12]wa_sel_3
+  //[13]wd_sel_1
+  //[14]wd_sel_2
+  //[15]wd_sel_3
+  //[16]sp_load
+  //[17]op1_sel_1
+  //[18]op2_sel_1
+  //[19]op2_sel_2
+  //[20]op2_sel_3
+  //[21]op2_sel_4
+  //[22:25]alu_oprn
+  //[26]ma_sel_1
+  //[27]ma_sel_2
+  //[28]md_sel_1
+
+
 
 endmodule
 //------------------------------------------------------------------------------------------
