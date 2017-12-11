bb646affec54304c103a214287bd0b6708291522
firen777 <hivemindac1995@gmail.com>
2017-12-11 01:36:51 -0800

    reg file draft not tested

--- IVtest/register_file.v
+++ IVtest/register_file.v
@@ -36,17 +36,60 @@
 module REGISTER_FILE_32x32(DATA_R1, DATA_R2, ADDR_R1, ADDR_R2,
                             DATA_W, ADDR_W, READ, WRITE, CLK, RST);
 
-// input list
-input READ, WRITE, CLK, RST;
-input [`DATA_INDEX_LIMIT:0] DATA_W;
-input [`REG_ADDR_INDEX_LIMIT:0] ADDR_R1, ADDR_R2, ADDR_W;
+  // input list
+  input READ, WRITE, CLK, RST;
+  input [`DATA_INDEX_LIMIT:0] DATA_W;
+  input [`REG_ADDR_INDEX_LIMIT:0] ADDR_R1, ADDR_R2, ADDR_W;
 
-// output list
-output [`DATA_INDEX_LIMIT:0] DATA_R1;
-output [`DATA_INDEX_LIMIT:0] DATA_R2;
+  // output list
+  output [`DATA_INDEX_LIMIT:0] DATA_R1;
+  output [`DATA_INDEX_LIMIT:0] DATA_R2;
 
-// TBD
+  wire nRST;    //!RST, such that reset on 1.
+  wire [`DATA_INDEX_LIMIT:0] w_5x32;    //output of 5x32 decoder
+  wire [`DATA_INDEX_LIMIT:0] w_load;    //output of w_5x32 & WRITE
+  wire [`DATA_INDEX_LIMIT:0] rQ [`DATA_INDEX_LIMIT:0]; //Q of reg32s
+  wire [`DATA_INDEX_LIMIT:0] w_r1;      //ADDR_R1
+  wire [`DATA_INDEX_LIMIT:0] w_r2;      //ADDR_R2
+  genvar i;
 
+  //decode write address
+  DECODER_5x32 DECODER_5x32_INST(.D(w_5x32), .I(ADDR_W));
+  generate
+    for(i=0;i<32;i=i+1)
+    begin: register_file_w_decode_gen
+      and and_inst(w_load[i], w_5x32[i], WRITE);
+    end
+  endgenerate
+
+  //init 32x reg32
+  not not_inst(nRST, RST);
+  generate
+    for(i=0;i<32;i=i+1)
+    begin: register_file_reg32_gen
+      REG32 REG32_INST(.Q(rQ[i]),
+                       .D(DATA_W),
+                       .LOAD(w_load[i]),
+                       .CLK(CLK),
+                       .RESET(nRST));
+    end
+  endgenerate
+
+  //ADDR_R1 selection
+  MUX32_32x1 MUX32_32x1_INST1(w_r1, rQ[0], rQ[1], rQ[2], rQ[3], rQ[4], rQ[5], rQ[6], rQ[7],
+                                    rQ[8], rQ[9], rQ[10], rQ[11], rQ[12], rQ[13], rQ[14], rQ[15],
+                                    rQ[16], rQ[17], rQ[18], rQ[19], rQ[20], rQ[21], rQ[22], rQ[23],
+                                    rQ[24], rQ[25], rQ[26], rQ[27], rQ[28], rQ[29], rQ[30], rQ[31], ADDR_R1);
+  //ADDR_R2 selection
+  MUX32_32x1 MUX32_32x1_INST2(w_r2, rQ[0], rQ[1], rQ[2], rQ[3], rQ[4], rQ[5], rQ[6], rQ[7],
+                                    rQ[8], rQ[9], rQ[10], rQ[11], rQ[12], rQ[13], rQ[14], rQ[15],
+                                    rQ[16], rQ[17], rQ[18], rQ[19], rQ[20], rQ[21], rQ[22], rQ[23],
+                                    rQ[24], rQ[25], rQ[26], rQ[27], rQ[28], rQ[29], rQ[30], rQ[31], ADDR_R2);
+
+  //READ enable
+  MUX32_2x1 MUX32_2x1_INST1 (DATA_R1, 32'bZ, w_r1, READ);
+  MUX32_2x1 MUX32_2x1_INST2 (DATA_R2, 32'bZ, w_r2, READ);
+  // high Z: 32'bZ
 endmodule
 
 //------------------------------------------------------------------------------------------
